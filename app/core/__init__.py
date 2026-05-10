cd52d3d0b654ee14baaf6f3aa95a5e98fd1de2b6
lago60174-dot <lago60174@gmail.com>
2026-05-10 09:52:50 +0100

    Update __init__.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,6 +1,6 @@
-from .ema_pullback import EMAPullbackStrategy
-from .breakout_atr import BreakoutATRStrategy
-from .rsi_mean_reversion import RSIMeanReversionStrategy
+from .strategies.ema_pullback import EMAPullbackStrategy
+from .strategies.breakout_atr import BreakoutATRStrategy
+from .strategies.rsi_mean_reversion import RSIMeanReversionStrategy
 
 __all__ = [
     "EMAPullbackStrategy",
